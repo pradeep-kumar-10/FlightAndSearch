bd4065f7b90ee4320b80013c5cf9ec250bd05bae
Pradeep Kumar <pra45516@gmail.com>
2024-05-16 14:36:04 +0530

    Added Airplane model & seeders

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 
-const {Airport, City} = require('./models/index');
+const {Airplane} = require('./models/index');
 const db = require('./models/index');
 
 const setupAndStartServer = async() => {
@@ -17,25 +17,14 @@ const setupAndStartServer = async() => {
         console.log(`Server started at ${PORT}`);
     });
 
-    // const airports = await Airport.findAll();
-
-    // const airports = await Airport.findAll({ 
-    //     include: [{
-    //         model: City
-    //     }]
-    // });
+    await Airplane.create({
+        modelNumber: 'Bombardier CRJ'
+    })
 
     // After every association we need to do
     if(process.env.SYNC_DB){
         db.sequelize.sync({alter: true});
     }
-    
-    // const city = await City.findOne({
-    //     where: { id:10 }
-    // });
-    // const airports = await city.getAirports();
-    // console.log(city, airports);
-
 }
 
 setupAndStartServer();
